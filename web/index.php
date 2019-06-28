3de583981854e2a597ab6a9b53cdbbfade2008e8
Nguyen Tien Huy <13034759+collyn@users.noreply.github.com>
2019-06-28 22:19:56 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,25 +1,18 @@
 <?php
-
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+$url=$_GET['url'];
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, urldecode($url));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+$headers = array();
+$headers[] = 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36 OPR/62.0.3331.18';
+$headers[] = 'Referer: http://www.phimmoi.net/phim/sieu-anh-hung-shazam-i2-8085/xem-phim.html';
+$headers[] = 'Origin: http://www.phimmoi.net';
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
+print_r($result);
