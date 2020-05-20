80bdfa9d5366c18f701658a54559eb074df42044
Nguyen Tien Huy <13034759+collyn@users.noreply.github.com>
2020-05-20 13:38:43 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin', '*');
+header("Access-Control-Allow-Origin:*");
 $url=urldecode($_GET['url']);
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, urldecode($url));
